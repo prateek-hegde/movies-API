d8100780bc434b0d10aa20714b47b0810a8c0d46
Prateek Hegde <prateekhegde12@gmail.com>
2019-04-29 10:44:58 +0530

    some change

--- services/database.js
+++ services/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-// let db = 'mongodb://neo:D3v$@13.232.181.171:27017/movies'
+let db = 'mongodb://neo:D3v$@13.232.181.171:27017/movies'
 
 //database connection
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useCreateIndex: true }).then((e) => {
+mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true }).then((e) => {
   console.log('database connected');
 }).catch((err) => {
   console.log(err);
